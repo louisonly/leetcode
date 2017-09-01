e013ef16ae2b9ea6c8145b5dbffc75ae916dedf6
louisonly <louisonly@163.com>
2017-09-01 15:25:26 +0800

    Create Solution.java

---
+++ java/018-4Sum/Solution.java
@@ -0,0 +1,26 @@
+class Solution {
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        List<List<Integer>> res = new LinkedList<>();
+        Arrays.sort(nums);
+        for(int i = 0; i < nums.length -3 ; i++){
+            if(i == 0 || nums[i] != nums[i-1]){
+                for(int j = i + 1; j < nums.length - 2; j++){
+                    if(j == i + 1 || nums[j] != nums[j-1]){
+                        int start = j + 1, end = nums.length -1, value = target - nums[i] - nums[j];
+                        while(start < end){
+                            if(nums[start] + nums[end] == value){
+                                res.add(Arrays.asList(nums[i], nums[j], nums[start], nums[end]));
+                                while(start < end && nums[start] == nums[start+1]) start++;
+                                while(start < end && nums[end] == nums[end-1]) end--;
+                                start++; end--;
+                            }else if(nums[start] + nums[end] > value){
+                                end--;
+                            }else start++;
+                        }
+                   }
+                }
+            }
+        }
+        return res;
+    }
+}
